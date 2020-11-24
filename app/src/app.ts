2002328835eb820c6b30a067057faaf6d03d38c8
Derek Burkgren <derek.burkgren@gmail.com>
2020-11-23 21:22:43 -0800

    Add support for Reuben's Brews.

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,5 +1,7 @@
 import { Cloudburst } from './scrapers/cloudburst'
+import { Reubens } from './scrapers/reubens'
 
 Cloudburst.getBeers()
     .then(beers => console.log(beers))
-
+Reubens.getBeers()
+    .then(beers => console.log(beers))
---
+++ app/src/scrapers/reubens.ts
@@ -0,0 +1,96 @@
+import { Beer, FormFactor } from '../models/beer'
+import cheerio from 'cheerio'
+import got from 'got'
+
+async function getBeers() {
+    let {body} = await got('https://reubensbrews.com/tap-list-taproom/')
+
+    return parseBeers(body)
+}
+
+function parseBeers(data: string): Beer[] {
+    let $ = cheerio.load(data)
+
+    let parseCategory = (category: cheerio.Element) : Beer[] => {
+        let categoryName = 
+            $('.tap-list-category-block-title', category).text().trim()
+        let beers = 
+            $('.tap-list-beer', category)
+                .toArray()
+                .map(x => parseBeer(x, categoryName))
+
+        return beers
+    }
+
+    let parseBeer = (rawBeer: cheerio.Element, categoryName: string) : Beer => {
+        let name = $('.the-tl-beer-name', rawBeer).text().trim()
+        let abv = $('.tl-abv-ibu .tl-abv-number', rawBeer).text().trim()
+        let formFactors = getFormFactor(rawBeer)
+    
+        return {
+            name: name,
+            abv: parseInt(abv),
+            ibu: undefined,
+            style: categoryName,
+            formFactor: formFactors.map(sanitizeFormFactor)
+        }
+    }
+
+    let getFormFactor = (formatData: cheerio.Element) => {
+        if (!formatData) {
+            return []
+        }            
+        
+        return $('.tl-vessel-type', formatData)
+            .children('.holder')
+            .toArray()
+            .map(x => x.attribs["class"].split(' ')[0])
+    }
+
+    let isBeerCategory = (category: cheerio.Element) : boolean => {
+        let categoryName = 
+            $('.tap-list-category-block-title', category).text().trim()
+    
+        switch(categoryName.toUpperCase()) {
+            case "FRUITFIZZ HARD SELTZER":
+            case "CIDER":
+                return false
+            default:
+                return true
+        }
+    }
+
+    let categories = 
+        $('.the-tap-list div.tap-list-beer-category-block').toArray()
+    
+    return categories
+        .filter(isBeerCategory)
+        .flatMap(parseCategory)    
+}
+
+function removeUndefined(input: (string | undefined)[]) : string[] {
+    const result : string[] = []
+    input.forEach(x => {
+        if (x) {
+            result.push(x)
+        }
+    })
+    return result
+}
+
+function sanitizeFormFactor(input : string | undefined) {
+    switch (input) {
+        case 'bottle-holder':
+            return FormFactor.TallBottle
+        case 'can-holder':
+            return FormFactor.FourPack;
+        case 'growler-holder':
+            return FormFactor.Growler;
+        default:
+            return FormFactor.Crowler;
+    }
+}
+
+export const Reubens = {
+    getBeers
+}
