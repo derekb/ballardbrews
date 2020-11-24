9cbe64f978479e6cb1f39491be35d5bc61708b03
Derek Burkgren <derek.burkgren@gmail.com>
2020-11-23 23:10:58 -0800

    Add express server.

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,12 +1,24 @@
 import { BreweryScraper } from "./scrapers/BreweryScraper";
 import { Cloudburst } from "./scrapers/Cloudburst";
 import { Reubens } from "./scrapers/Reubens";
+import express from "express";
 
 let cloudburst = new Cloudburst();
 let reubens = new Reubens();
 
 let scrapers: BreweryScraper[] = [cloudburst, reubens];
 
-Promise.all(scrapers.flatMap((x) => x.getBeers()))
-  .then((beers) => beers.flatMap((x) => x))
-  .then((beer) => console.log(beer));
+const app = express();
+const port = 8080;
+
+app.get("/", async (req, res) => {
+  let beers = await Promise.all(
+    scrapers.flatMap((x) => x.getBeers())
+  ).then((beers) => beers.flatMap((x) => x));
+
+  res.send(beers);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
